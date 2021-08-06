ab776c5ed384178142cd8daa36d179fb8a157eeb
Moin11 <moin11.dev@gmail.com>
2021-08-07 03:34:49 +0530

    implemented redux and state management commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import { CONNECTION_URI } from "./URIconfig.js"; // create a config file to store your connection credentials/URI
+import postroutes from "../server/routes/posts.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postroutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(PORT));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+mongoose.set(`useFindAndModify`, false);
