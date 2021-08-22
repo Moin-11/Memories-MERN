4a582214939f91c2ebd003b65d7115a63488dfe9
Moin11 <moin11.dev@gmail.com>
2021-08-23 01:46:23 +0530

    refactorings

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -29,7 +29,8 @@ const Navbar = () => {
     if (token) {
       const decodedToken = decode(token);
 
-      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+      if (decodedToken.exp * 1000 < new Date().getTime()) 
+      {logout();}
     }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-
+//
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -18,12 +18,8 @@ app.use(cors());
 app.use("/posts", postroutes);
 app.use("/user", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Memories API");
-});
-
 const PORT = process.env.PORT || 5000;
-
+//
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
