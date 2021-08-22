0da36073f69f8ebed025ef112a01f1ed3610930e
Moin11 <moin11.dev@gmail.com>
2021-08-22 17:26:39 +0530

    implemented AUTH middleware and refactored API

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -54,10 +54,22 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id } = req.params;
 
+  if (!req.userId) {
+    return res.json({ message: "action not authorized" });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
   const post = await PostMessage.findById(id);
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes.filter((id) => id != String(req.userId));
+  }
 
   const updatedPost = await PostMessage.findByIdAndUpdate(
     id,
